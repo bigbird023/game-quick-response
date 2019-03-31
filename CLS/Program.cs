29081dbf60362d7faa3f11a48d9e4ce5c220015a
Craig Benner <cpb113@psu.edu>
2019-03-31 11:06:07 -0400

    ready for tutorial step 1

---
+++ CLS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace game_quick_response
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Quick Response!");
+
+            Console.WriteLine("Please Choose a Game Level (E)asy, (M)edicore, or (H)ard?");
+            int inputRead = Console.Read();
+            Char gameLevel = Convert.ToChar(inputRead);
+            Console.WriteLine(inputRead.ToString());
+            Thread.Sleep(1000);
+
+            Console.WriteLine("You Choose " + gameLevel);
+        }
+    }
+}
