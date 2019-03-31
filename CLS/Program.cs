d79e263143c7508b1af885ef6f966bc845f690f7
Craig Benner <cpb113@psu.edu>
2019-03-31 11:39:38 -0400

    adding logic to start game

--- CLS/Program.cs
+++ CLS/Program.cs
@@ -5,16 +5,60 @@ namespace game_quick_response
 {
     class Program
     {
+        static int gameSpeed = 1000;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Quick Response!");
 
-            Console.WriteLine("Please Choose a Game Level (E)asy, (M)edicore, or (H)ard?");
-            int inputRead = Console.Read();
-            Char gameLevel = Convert.ToChar(inputRead);
+            do {
+                retrieveGameLevel();
+                Thread.Sleep(1000);
 
-            Console.WriteLine("You Choose " + gameLevel);
-            Thread.Sleep(1000);
+            } while (continueGame());
+
+            
+        }
+
+        static void retrieveGameLevel() {
+            String inputGameLevel;
+            do {
+                Console.WriteLine("Please Choose a Game Level (E)asy, (M)edicore, or (H)ard?");
+                inputGameLevel = Console.ReadLine();
+                Console.WriteLine("You Choose " + inputGameLevel);
+                
+            } while (!(inputGameLevel.Equals("E") || inputGameLevel.Equals("M") || inputGameLevel.Equals("H")));
+
+            switch (inputGameLevel) {
+                case "E":
+                    gameSpeed = 1000;
+                    break;
+                case "M":
+                    gameSpeed = 500;
+                    break;
+                case "H":
+                    gameSpeed = 100;
+                    break;
+               }
+        }
+
+        static Boolean continueGame() {
+            String inputContinueGame;
+            do {
+                Console.WriteLine("(C)ontinue or (E)xit");
+                inputContinueGame = Console.ReadLine();
+
+                Console.WriteLine("You Choose " + inputContinueGame);
+                
+            } while (!inputContinueGame.Equals("C") && !inputContinueGame.Equals("E"));
+
+            Boolean ret;
+            if (inputContinueGame.Equals("C")) {
+                ret = true;
+            } else {
+                ret = false;
+            }
+            return ret;
         }
     }
 }
